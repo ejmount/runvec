3114066678caf52e115852a0ad041578771165f0
Ewan Mount <ejmount@users.noreply.github.com>
2019-06-15 02:27:31 +0100

    Moved tests

--- src/iter.rs
+++ src/iter.rs
@@ -99,3 +99,24 @@ where
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::iter::*;
+    #[test]
+    fn runiter_test() {
+        let expected = vec![(1, 3), (5, 1), (6, 1), (3, 1), (2, 3)];
+        let test_items = vec![1, 1, 1, 5, 6, 3, 2, 2, 2];
+        let ri = RunLengthIterator::new(test_items.into_iter());
+        assert_eq!(ri.collect::<Vec<_>>(), expected);
+    }
+
+    #[test]
+    fn expandingiter_test() {
+        let expected = vec![1, 1, 1, 2, 3, 3];
+        let test_items = vec![(1, 3), (2, 1), (3, 2)];
+        let ri = ExpandingIterator::new(test_items.into_iter());
+        assert_eq!(ri.collect::<Vec<_>>(), expected);
+    }
+
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,5 @@
 mod iter;
 mod runvec;
 
+pub use crate::iter::{ExpandingIterator, RunLengthIterator};
 pub use crate::runvec::{RunLenCompressible, RunLenVec};
-pub use iter::{ExpandingIterator, RunLengthIterator};
-
-#[cfg(test)]
-mod tests {
-    use crate::iter::*;
-    #[test]
-    fn runiter_test() {
-        let expected = vec![(1, 3), (5, 1), (6, 1), (3, 1), (2, 3)];
-        let test_items = vec![1, 1, 1, 5, 6, 3, 2, 2, 2];
-        let ri = RunLengthIterator::new(test_items.into_iter());
-        assert_eq!(ri.collect::<Vec<_>>(), expected);
-    }
-
-    #[test]
-    fn expandingiter_test() {
-        let expected = vec![1, 1, 1, 2, 3, 3];
-        let test_items = vec![(1, 3), (2, 1), (3, 2)];
-        let ri = ExpandingIterator::new(test_items.into_iter());
-        assert_eq!(ri.collect::<Vec<_>>(), expected);
-    }
-
-}
